2c36e3b0dca744185ae91b0d6f2496b7291d81c6
zrpxx <zrpxx@qq.com>
2025-12-06 15:07:46 +0800

    update config

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,8 @@ module.exports = {
         {
             name: 'TextMahjong',
             port: '4000',
-            exec_mode: 'cluster',
-            instances: 'max',
+            exec_mode: 'fork',
+            instances: 1,
             script: './.output/server/index.mjs',
         },
     ],
